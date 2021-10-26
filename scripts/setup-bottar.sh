e64df048ba7b7dd6b4962ffd7d98e70551452887
Zaid Akram <hello@zaidakram.com>
2021-10-27 01:31:57 +0500

    We don't know the name of equivalent www folder name in outsystems.
    So lets copy the files over to platform folder manually.

--- scripts/setup-bottar.sh
+++ scripts/setup-bottar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp source/plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js source/www/js/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
-cp source/plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js source/www/js/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
+cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js platforms/android/app/src/main/assets/www/js/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
+cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js platforms/android/app/src/main/assets/www/js/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
