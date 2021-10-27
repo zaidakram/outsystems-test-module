49695cf4810bbfb30fd729f9861e8aecbf8bc34a
Zaid Akram <hello@zaidakram.com>
2021-10-27 18:42:31 +0500

    Cordova deletes the files which don't have a source on merge. Lets try to put these in source.

--- scripts/setup-bottar.sh
+++ scripts/setup-bottar.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js platforms/android/app/src/main/assets/www/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
-cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js platforms/android/app/src/main/assets/www/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
+# cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js platforms/android/app/src/main/assets/www/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
+# cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js platforms/android/app/src/main/assets/www/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
+
+cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
+cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
