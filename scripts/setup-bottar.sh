2c2a20c96567763a3d7df1df1625af663670adf2
Zaid Akram <hello@zaidakram.com>
2021-10-29 01:12:43 +0500

    Fixed manifest override

--- scripts/setup-bottar.sh
+++ scripts/setup-bottar.sh
@@ -6,5 +6,9 @@
 cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
 cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
 
-./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js" = "?123"' platforms/android/app/src/main/assets/www/manifest.json
-./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js" = "?321"' platforms/android/app/src/main/assets/www/manifest.json
+./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js" = "?123"' platforms/android/app/src/main/assets/www/manifest.json > transformed.json
+./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js" = "?321"' transformed.json > transformed-final.json
+
+rm transformed.json
+rm platforms/android/app/src/main/assets/www/manifest.json
+mv transformed-final.json platforms/android/app/src/main/assets/www/manifest.json
