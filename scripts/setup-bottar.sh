6f4a309f32654031ecf69846b6ab04ff55abbf77
Zaid Akram <hello@zaidakram.com>
2021-10-29 04:17:11 +0500

    Replace manifest hash with a new hash and use that in cached version

--- scripts/setup-bottar.sh
+++ scripts/setup-bottar.sh
@@ -6,12 +6,12 @@
 cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
 cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
 
-./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js" = "?123"' www/manifest.json > transformed.json
-cat transformed.json
-./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js" = "?321"' transformed.json > transformed-final.json
-cat transformed-final.json
+random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 22 | head -n 1)
+json_sub=".manifest.versionToken = \""$random_string"\" | .manifest.urlVersions.\"/Chat/manifest.json\" = \""?$random_string"\" | .manifest.urlMappings.\"/Chat/moduleservices/moduleinfo?cached\" = \""/Chat/manifest.json?$random_string"\" | .manifest.urlVersions.\"/Chat/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js\" = \"?123\" | .manifest.urlVersions.\"/Chat/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js\" = \"?321\""
 
-rm transformed.json
+./plugins/bottar-ai/bin/jq "${json_sub}" www/manifest.json > transformed.json
+cat transformed.json
+
 rm www/manifest.json
-mv transformed-final.json www/manifest.json
+mv transformed.json www/manifest.json
 echo "Updated manifest.json with CXI SDK scripts!!"
