b52822c6d1f73b6be59d75508567adc3073ba233
Zaid Akram <hello@zaidakram.com>
2021-10-29 01:52:10 +0500

    Update the manifest in source instead of platform

--- scripts/setup-bottar.sh
+++ scripts/setup-bottar.sh
@@ -6,9 +6,12 @@
 cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
 cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
 
-./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js" = "?123"' platforms/android/app/src/main/assets/www/manifest.json > transformed.json
+./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js" = "?123"' www/manifest.json > transformed.json
+cat transformed.json
 ./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js" = "?321"' transformed.json > transformed-final.json
+cat transformed-final.json
 
 rm transformed.json
-rm platforms/android/app/src/main/assets/www/manifest.json
-mv transformed-final.json platforms/android/app/src/main/assets/www/manifest.json
+rm www/manifest.json
+mv transformed-final.json www/manifest.json
+echo "Updated manifest.json with CXI SDK scripts!!"
