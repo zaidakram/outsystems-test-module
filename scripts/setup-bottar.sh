283f32e02e09bc4586278b1480732a2135a7ca69
Zaid Akram <hello@zaidakram.com>
2021-10-29 00:43:06 +0500

    Manually add cxi files to the outsystems manifest.json file with jq

--- scripts/setup-bottar.sh
+++ scripts/setup-bottar.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js platforms/android/app/src/main/assets/www/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
-# cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js platforms/android/app/src/main/assets/www/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
+# cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js www/js/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
+# cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js www/js/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
 
 cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js
 cp plugins/bottar-ai/dist/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js www/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js
+
+./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker-webrtc_client_StandardWebRTCMessageBrokerClient_js.main.js" = "?123"' platforms/android/app/src/main/assets/www/manifest.json
+./plugins/bottar-ai/bin/jq '.manifest.urlVersions."/Chat/scripts/cxi-message-broker-client_message-broker_client_websocket_StandardWSMessageBrokerClient_js.main.js" = "?321"' platforms/android/app/src/main/assets/www/manifest.json
